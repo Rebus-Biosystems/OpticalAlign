3485f66ff613248a33dfe57f63d3abda3534a82c
syousefi@rebusbio.com <syousefi@rebusbio.com>
2022-11-04 17:12:12 -0400

    Update OpticalAlign.bat

--- OpticalAlign.bat
+++ OpticalAlign.bat
@@ -1,11 +1,11 @@
 
-set PYTHON_CALL="../env_oa/Scripts/python.exe" 
+set PYTHON_CALL="../../env_oa/Scripts/python.exe" 
 
 @ECHO OFF 
 :: This batch file to open the Optical Align Visualization  
 TITLE Esper Optical Align V1.0
 
-ECHO RUNNING SPOT TABLE VISUALIZATION
+ECHO RUNNING Optical_Align v1.0 software 
 %PYTHON_CALL% "../OpticalAlign/src/svbeammeasure_v10/app.py"
 
 ECHO Finished Optical Align V1.0 ... 
